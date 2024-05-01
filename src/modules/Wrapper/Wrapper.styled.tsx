7e852516fae70af0ef9b86adf69ce0d881843471
dimamaksymiuk01 <admin@admins-MacBook-Air.local>
2024-05-01 14:09:09 +0300

    complete mobile version

--- src/modules/Wrapper/Wrapper.styled.tsx
+++ src/modules/Wrapper/Wrapper.styled.tsx
@@ -4,9 +4,14 @@
 import styled from 'styled-components';
 
 export const WrapperStyled = styled.div`
-  margin: 30px;
+  padding: 30px;
   display: flex;
   justify-content: space-between;
   max-width: 1440px;
   gap: clamp(0.625rem, -8.446rem + 18.9vw, 8.563rem);
+
+  @media (max-width: 845px) {
+    justify-content: center;
+    width: 100%;
+  }
 `;
